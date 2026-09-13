a7af697346466b8f9ddeb0f4633d56c76d79ec29
zahar <zaharrogoff@gmail.com>
2026-09-13 21:14:23 +0700

    Add DemoText

--- app/src/main/java/com/example/lr1/MainActivity.kt
+++ app/src/main/java/com/example/lr1/MainActivity.kt
@@ -7,17 +7,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.Composable
 import com.example.lr1.ui.theme.Lr1Theme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
 
         setContent {
             Lr1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+
                     DemoScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -25,4 +34,19 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+
+@Composable
+fun DemoText(
+    message: String,
+    fontSize: Float
+) {
+
+    Text(
+        text = message,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold
+    )
+
 }
