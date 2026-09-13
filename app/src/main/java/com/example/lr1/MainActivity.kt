e042e8e7676a0e9bd8086f8cefd0f6aec67b0db1
zahar <zaharrogoff@gmail.com>
2026-09-13 21:15:08 +0700

    Add Slider

--- app/src/main/java/com/example/lr1/MainActivity.kt
+++ app/src/main/java/com/example/lr1/MainActivity.kt
@@ -7,22 +7,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import com.example.lr1.ui.theme.Lr1Theme
+
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
 
         setContent {
+
             Lr1Theme {
+
                 Scaffold(
                     modifier = Modifier.fillMaxSize()
                 ) { innerPadding ->
@@ -30,11 +36,17 @@ class MainActivity : ComponentActivity() {
                     DemoScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
+
             }
+
         }
+
     }
+
 }
+
 
 
 @Composable
@@ -48,5 +60,36 @@ fun DemoText(
         fontSize = fontSize.sp,
         fontWeight = FontWeight.Bold
     )
+
+}
+
+
+
+@Composable
+fun DemoSlider(
+    sliderPosition: Float,
+    onPositionChange: (Float) -> Unit
+) {
+
+    Slider(
+
+        modifier = Modifier.padding(10.dp),
+
+        valueRange = 20f..38f,
+
+        value = sliderPosition,
+
+        onValueChange = onPositionChange
+
+    )
+
+}
+
+
+
+@Composable
+fun DemoScreen(
+    modifier: Modifier = Modifier
+) {
 
 }
