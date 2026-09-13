b25d6e1877bfaaac294babe078708480d35586c8
zahar <zaharrogoff@gmail.com>
2026-09-13 21:15:41 +0700

    Create DemoScreen

--- app/src/main/java/com/example/lr1/MainActivity.kt
+++ app/src/main/java/com/example/lr1/MainActivity.kt
@@ -4,17 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lr1.ui.theme.Lr1Theme
+
 
 
 class MainActivity : ComponentActivity() {
@@ -91,5 +102,72 @@ fun DemoSlider(
 fun DemoScreen(
     modifier: Modifier = Modifier
 ) {
+
+
+    var sliderPosition by remember {
+
+        mutableFloatStateOf(20f)
+
+    }
+
+
+
+    val handlePositionChange = { position: Float ->
+
+        sliderPosition = position
+
+    }
+
+
+
+    Column(
+
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        verticalArrangement = Arrangement.Center,
+
+        modifier = Modifier.fillMaxSize()
+
+    ) {
+
+
+        DemoText(
+
+            message = "Welcome to Compose",
+
+            fontSize = sliderPosition
+
+        )
+
+
+
+        Spacer(
+
+            modifier = Modifier.height(150.dp)
+
+        )
+
+
+
+        DemoSlider(
+
+            sliderPosition = sliderPosition,
+
+            onPositionChange = handlePositionChange
+
+        )
+
+
+
+        Text(
+
+            style = MaterialTheme.typography.headlineMedium,
+
+            text = sliderPosition.toInt().toString() + "sp"
+
+        )
+
+
+    }
 
 }
